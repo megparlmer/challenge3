7c1c27c2faeef81695654d7803b327c312ca07de
megparlmer <megparlmer@gmail.com>
2022-03-05 21:10:14 -0500

    added if statements to make application functional

--- Develop/script.js
+++ Develop/script.js
@@ -1,41 +1,103 @@
 // Assignment code here
 //establishing character variables
-var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numericChars = "123456789";
-var specialChars = "!@#$%^&*()";
-
-//defining generatePassword
-function generatePassword() {
-  var password = "";
-  var passwordChars = "";
-
-  var numChars = prompt()
-
-  //prompting user to choose length of password
-  var numChars = window.prompt("choose length of password");
-  if (numChars < 8 || numChars > 128) {
-    return
-  }
-
-  var lowercaseChars 
-  //input should be validated, one character type should be chosen
-  //generate password
-  return("temp password");
+var keys = {
+  lowercaseChars: "abcdefghijklmnopqrstuvwxyz",
+  uppercaseChars: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  numericChars: "123456789",
+  specialChars: "!@#$%^&*()"
 }
 
-//function generatePassword() {
-  //console.log("button has been clicked");
+//defining generatePassword
+var generatePassword = function() {
+  window.alert("generate a password")
 
-  //var passwordPrompt = window.prompt("Enter Password");
-  //if (passwordPrompt === "" || passwordPrompt === null) {
-    //window.alert("Provide legit password!");
-    //return generatePassword();
-  //}
+  //prompting user to choose length of password
+  var numChars = window.prompt("choose length of password between 8 and 128");
+  console.log(numChars);
+  if (numChars < 8 || numChars > 128) {
+    window.alert("enter valid validity");
+    return "";
+  } 
+
+  //prompting user to see if they want to use lowercase
+  var useLowercase = false
+  var lowercase = window.prompt("Do you want lowercase? Enter 'YES' or 'NO'");
+  if (lowercase === "YES") {
+    useLowercase = true;
+    return "";
+  }
+  
+  //prompting user to see if they want to use uppercase
+  var useUppercase = false
+  var uppercase = window.prompt("Do you want to uppercase? Enter 'YES' or 'NO'");
+  if(uppercase === "YES") {
+    useUppercase = true;
+    return "";
+  }
+
+  //prompting for special characters
+  var useSpecialChar = false
+  var specialChar = window.prompt("Do you want some special characters? Enter 'YES' or 'NO'");
+  if(specialChar === "YES") {
+    useSpecialChar = true;
+    return "";
+  }
+
+  //prompting for numeric
+  var useNumericChar = false
+  var numericChar = window.prompt("Do you want some numeric characters? Enter 'YES' or 'NO'");
+  if(numericChar === "YES") {
+    useNumericChar = true;
+    return "";
+  }
+
+  //done with input
+  var randomPassword = "";
+  var numCharacterTypes = 0
+  var combineChars = "";
+
+  //establishing if statements for the key variables
+  if (useLowercase) {
+    combineChars = combineChars.concat(keys.lowercaseChars);
+    var randomIndex = Math.floor(Math.random() * keys.lowercaseChars.length);
+    randomPassword = randomPassword + keys.lowercaseChars[randomIndex];
+    numCharacterTypes++;
+  }
+
+  if (useUppercase) {
+    combineChars = combineChars.concat(keys.uppercaseChars);
+    var randomIndex = Math.floor(Math.random() * keys.uppercaseChars.length);
+    randomPassword = randomPassword + keys.uppercaseChars[randomIndex];
+    numCharacterTypes++;
+  }
+
+  if (useSpecialChar) {
+    combineChars = combineChars.concat(keys.specialChars);
+    var randomIndex = Math.floor(Math.random() * keys.specialChars.length);
+    randomPassword = randomPassword + keys.specialChars[randomIndex];
+    numCharacterTypes++;
+  }
+
+  if (useNumericChar) {
+    combineChars = combineChars.concat(keys.numericChars);
+    var randomIndex = Math.floor(Math.random() * keys.specialChars.length);
+    randomPassword = randomPassword + keys.specialChars[randomIndex];
+    numCharacterTypes++;
+  }
+
+  if (numCharacterTypes === 0) {
+    window.alert("choose again weenie");
+    return"";
+  }
+
+  for(var i = 0; i < numberChars - numCharacterTypes; i++) {
+    var randomIndex = Math.floor(Math.random() * combineCharacters.length);
+    randomPassword = randomPassword + combineCharacters[randomIndex];
+  }
+  return randomPassword;
+}
 
 
-  //return "Generated  password will go here";
-//}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
